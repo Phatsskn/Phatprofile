bceceeaa3aa397762c57aef5862a9d9e5047da65
0xDgtl <104255050+0xDgtl@users.noreply.github.com>
2025-02-02 02:03:56 +0000

    Added new images and folders

---
+++ js/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          "primary": "#E42278",
+          "secondary": "#ED7BAB",
+          "accent": "#4BC7BC",
+          "neutral": "#F5D3DD",
+          "base-100": "#F9F9F9",
+        },
+      },
+    ],
+  },
+};
